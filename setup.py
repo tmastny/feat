eb2fc9bdd6604016b423ac6b820a175e664ab6b6
Timothy Mastny <tim.mastny@gmail.com>
2020-07-01 16:57:50 -0500

    adding packaging

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="feat",
+    version="0.0.1",
+    author="Tim Mastny",
+    author_email="tim.mastny@gmail.com",
+    description="Python package to make it easy to pull variable names and importance.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
