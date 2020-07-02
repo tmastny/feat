5d3312f5fdae5111b1759486a96ca49634ae9e79
Timothy Mastny <tim.mastny@gmail.com>
2020-07-02 12:05:33 -0500

    dispatch and tests working

--- feat/__init__.py
+++ feat/__init__.py
@@ -1,2 +1,24 @@
-def feat():
-    return "hello world!"
+from functools import singledispatch
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+import pandas as pd
+import numpy as np
+
+@singledispatch
+def feat(transformer, columns):
+    return "default"
+
+@feat.register(OneHotEncoder)
+def _(transformer, columns):
+    n_columns = np.array([len(category) for category in transformer.categories_])
+    if transformer.drop:
+        n_columns = n_columns - 1
+
+    name = pd.Series(columns).repeat(n_columns).reset_index(drop=True)
+
+    return pd.DataFrame({'name': name, 'feature': transformer.get_feature_names()})
+
+@feat.register(StandardScaler)
+def _(transformer, columns)
+    return pd.DataFrame({'name': columns, 'feature': columns})
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,4 +1,50 @@
-import feat
+import os
+import pandas as pd
+from feat import feat
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.compose import make_column_transformer
 
-def test_func():
-    assert "hello world" == feat.feat()
+dir = os.path.dirname(os.path.abspath(__file__))
+
+hotels = pd.read_csv(os.path.join(dir, "hotels.csv"))
+y = hotels['children']
+X = hotels.drop('children', axis=1)
+
+nominal = ['hotel', 'meal']
+numeric = ['lead_time', 'average_daily_rate']
+
+
+def test_OneHotEncoder():
+
+    expected = pd.DataFrame({
+        "name": ['hotel', 'hotel', 'meal', 'meal', 'meal'],
+        "feature": ["x0_City_Hotel", "x0_Resort_Hotel", "x1_BB", "x1_HB", "x1_SC"]
+    })
+
+    ohe = OneHotEncoder(sparse=False)
+
+    ohe.fit(hotels[nominal])
+
+    assert feat(ohe, nominal).equals(expected)
+
+def test_OneHotEncoder_drop():
+
+    expected = pd.DataFrame({
+        "name": ['hotel', 'meal', 'meal'],
+        "feature": ["x0_Resort_Hotel", "x1_HB", "x1_SC"]
+    })
+
+    ohe = OneHotEncoder(sparse=False, drop='first')
+
+    ohe.fit(hotels[nominal])
+
+    assert feat(ohe, nominal).equals(expected)
+
+
+def test_default():
+
+    expected = "default"
+
+    assert feat(1, 1) == expected
