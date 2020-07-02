6a9865b8692413b62f711f7514b4f7ac3141b8b4
Timothy Mastny <tim.mastny@gmail.com>
2020-07-02 13:22:39 -0500

    formating and default

--- feat/__init__.py
+++ feat/__init__.py
@@ -9,36 +9,34 @@ from typing import Union
 import pandas as pd
 import numpy as np
 
+
 @singledispatch
 def feat(transformer, columns):
-    return "default"
+    return pd.DataFrame({"name": columns, "feature": columns})
+
 
 @feat.register(OneHotEncoder)
-def _(transformer : OneHotEncoder, columns):
+def _(transformer: OneHotEncoder, columns):
     n_columns = np.array([len(category) for category in transformer.categories_])
     if transformer.drop:
         n_columns = n_columns - 1
 
     name = pd.Series(columns).repeat(n_columns).reset_index(drop=True)
 
-    return pd.DataFrame({'name': name, 'feature': transformer.get_feature_names()})
+    return pd.DataFrame({"name": name, "feature": transformer.get_feature_names()})
 
-@feat.register(OrdinalEncoder)
-@feat.register(StandardScaler)
-def _(transformer : Union[OrdinalEncoder, StandardScaler], columns):
-    return pd.DataFrame({'name': columns, 'feature': columns})
 
 @feat.register(KBinsDiscretizer)
-def _(transformer : KBinsDiscretizer, columns):
-    if transformer.encode == 'ordinal':
-        return pd.DataFrame({'name': columns, 'feature': columns})
+def _(transformer: KBinsDiscretizer, columns):
+    if transformer.encode == "ordinal":
+        return feat(None, columns)
 
     name = pd.Series(columns).repeat(transformer.n_bins_).reset_index(drop=True)
 
     edge_labels = [label[1:] for label in transformer.bin_edges_]
     feature = pd.concat(list(map(pd.Series, edge_labels))).reset_index(drop=True)
 
-    df = pd.DataFrame({'name': name, 'feature': feature})
-    df['feature'] = df['name'] + "-" + df['feature'].apply(lambda x: str(round(x)))
+    df = pd.DataFrame({"name": name, "feature": feature})
+    df["feature"] = df["name"] + "-" + df["feature"].apply(lambda x: str(round(x)))
 
     return df
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -12,25 +12,28 @@ from sklearn.compose import make_column_transformer
 dir = os.path.dirname(os.path.abspath(__file__))
 
 hotels = pd.read_csv(os.path.join(dir, "hotels.csv"))
-y = hotels['children']
-X = hotels.drop('children', axis=1)
+y = hotels["children"]
+X = hotels.drop("children", axis=1)
 
-nominal = ['hotel', 'meal']
-numeric = ['lead_time', 'average_daily_rate']
+nominal = ["hotel", "meal"]
+numeric = ["lead_time", "average_daily_rate"]
+
 
 def test_default():
 
-    expected = "default"
-
-    assert feat(1, 1) == expected
+    assert feat(None, nominal).equals(
+        pd.DataFrame({"name": nominal, "feature": nominal})
+    )
 
 
 def test_OneHotEncoder():
 
-    expected = pd.DataFrame({
-        "name": ['hotel', 'hotel', 'meal', 'meal', 'meal'],
-        "feature": ["x0_City_Hotel", "x0_Resort_Hotel", "x1_BB", "x1_HB", "x1_SC"]
-    })
+    expected = pd.DataFrame(
+        {
+            "name": ["hotel", "hotel", "meal", "meal", "meal"],
+            "feature": ["x0_City_Hotel", "x0_Resort_Hotel", "x1_BB", "x1_HB", "x1_SC"],
+        }
+    )
 
     ohe = OneHotEncoder(sparse=False)
 
@@ -38,18 +41,22 @@ def test_OneHotEncoder():
 
     assert feat(ohe, nominal).equals(expected)
 
+
 def test_OneHotEncoder_drop():
 
-    expected = pd.DataFrame({
-        "name": ['hotel', 'meal', 'meal'],
-        "feature": ["x0_Resort_Hotel", "x1_HB", "x1_SC"]
-    })
+    expected = pd.DataFrame(
+        {
+            "name": ["hotel", "meal", "meal"],
+            "feature": ["x0_Resort_Hotel", "x1_HB", "x1_SC"],
+        }
+    )
 
-    ohe = OneHotEncoder(sparse=False, drop='first')
+    ohe = OneHotEncoder(sparse=False, drop="first")
 
     ohe.fit(X[nominal])
 
     assert feat(ohe, nominal).equals(expected)
+
 
 def test_OrdinalEncoder():
     expected = pd.DataFrame({"name": nominal, "feature": nominal})
@@ -59,48 +66,56 @@ def test_OrdinalEncoder():
 
     assert feat(oe, nominal).equals(expected)
 
-kbin_expected = pd.DataFrame({
-        'name': {
-        0: 'lead_time',
-        1: 'lead_time',
-        2: 'lead_time',
-        3: 'lead_time',
-        4: 'lead_time',
-        5: 'average_daily_rate',
-        6: 'average_daily_rate',
-        7: 'average_daily_rate',
-        8: 'average_daily_rate',
-        9: 'average_daily_rate'},
-        'feature': {
-        0: 'lead_time-4',
-        1: 'lead_time-36',
-        2: 'lead_time-87',
-        3: 'lead_time-171',
-        4: 'lead_time-457',
-        5: 'average_daily_rate-69',
-        6: 'average_daily_rate-94',
-        7: 'average_daily_rate-123',
-        8: 'average_daily_rate-162',
-        9: 'average_daily_rate-335'
-    }})
+
+kbin_expected = pd.DataFrame(
+    {
+        "name": {
+            0: "lead_time",
+            1: "lead_time",
+            2: "lead_time",
+            3: "lead_time",
+            4: "lead_time",
+            5: "average_daily_rate",
+            6: "average_daily_rate",
+            7: "average_daily_rate",
+            8: "average_daily_rate",
+            9: "average_daily_rate",
+        },
+        "feature": {
+            0: "lead_time-4",
+            1: "lead_time-36",
+            2: "lead_time-87",
+            3: "lead_time-171",
+            4: "lead_time-457",
+            5: "average_daily_rate-69",
+            6: "average_daily_rate-94",
+            7: "average_daily_rate-123",
+            8: "average_daily_rate-162",
+            9: "average_daily_rate-335",
+        },
+    }
+)
+
 
 def test_KBinsDiscretizer():
-    kbin = KBinsDiscretizer(n_bins=5, encode='onehot')
+    kbin = KBinsDiscretizer(n_bins=5, encode="onehot")
     kbin.fit(X[numeric])
 
     assert feat(kbin, numeric).equals(kbin_expected)
+
 
 def test_KBinsDiscretizer_dense():
 
-    kbin = KBinsDiscretizer(n_bins=5, encode='onehot-dense')
+    kbin = KBinsDiscretizer(n_bins=5, encode="onehot-dense")
     kbin.fit(X[numeric])
 
     assert feat(kbin, numeric).equals(kbin_expected)
+
 
 def test_KBinsDiscretizer_ordinal():
     expected = pd.DataFrame({"name": numeric, "feature": numeric})
 
-    kbin = KBinsDiscretizer(n_bins=5, encode='ordinal')
+    kbin = KBinsDiscretizer(n_bins=5, encode="ordinal")
     kbin.fit(X[numeric])
 
     assert feat(kbin, numeric).equals(expected)
